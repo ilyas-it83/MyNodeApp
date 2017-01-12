1272490fbf5b896c2b6bdb508654d38b6b495817
Ilyas F <it83.mohammed@gmail.com>
2017-01-12 18:14:46 +0530

    C

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:8080");
+var server = supertest.agent("http://40.71.46.93:8080/");
 
 // UNIT test begin
 
